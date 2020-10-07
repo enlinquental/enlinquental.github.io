65439a106feb0f2caf59de9ceae8b8350fcbef80
Enlin Quental <42779486+enlinquental@users.noreply.github.com>
2020-10-07 22:25:08 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -92,9 +92,9 @@ window.onload = function(){
         var i, layer = document.querySelectorAll('.layer');
         for(i = 0; i < layer.length; i++){
             var speed = layer[i].getAttribute('data-speed');
-            var xX = (x * speed)/15;//100;
-            var yY = (y * speed)/13;//70;
-            layer[i].style.transform = 'translateX(' + xX + 'px) translateY(' + yY + 'px)';
+            var xX = (window.innerWidth - (x * speed))/200 + 5;
+            var yY = (window.innerHeight - (y * speed))/200;
+            layer[i].style.transform = 'translateX(' + xX + 'vw) translateY(' + yY + 'vh)';
         }
     };
 
